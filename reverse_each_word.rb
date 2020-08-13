291195c3095ec65ea7b5b5f247850bbce6072216
ZHENGJIAN LIU <comezheng@gmail.com>
2020-08-13 17:11:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(str)
   array = str.split(" ")
-  str.collect{|element| element.reverse}
-  str.join(" ")
+  array = array.map{|element| element.reverse}
+  array.join(" ")
 end
